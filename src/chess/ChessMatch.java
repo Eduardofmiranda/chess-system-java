c7b403b45f0e785df42cadd163d5cb808b93ae50
Eduardo Miranda <miranda_eduardo.freitas@hotmail.com>
2022-11-10 13:29:55 -0300

    Correção - melhoria para promotion

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -119,19 +118,19 @@ public class ChessMatch {
 			throw new IllegalStateException("Ther is no piece to be promoted");
 		}
 		if (!type.equals("B") && !type.equals("M") && !type.equals("R") && !type.equals("Q")) {
-			throw new InvalidParameterException("Invalid type for promotion");
+			return promoted;
 		}
 
 		Position pos = promoted.getChessPosition().toPosition();
 		Piece p = board.removePiece(pos);
 		piecesOnTheBoard.remove(p);
-		
+
 		ChessPiece newPiece = newPiece(type, promoted.getColor());
 		board.placePiece(newPiece, pos);
 		piecesOnTheBoard.add(newPiece);
-		
+
 		return newPiece;
-		
+
 	}
 
 	private ChessPiece newPiece(String type, Color color) {
